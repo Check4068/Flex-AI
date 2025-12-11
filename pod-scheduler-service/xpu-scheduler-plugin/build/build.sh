35835d3513699913a07c395c24bf26c689a81ed5
liuwenyu <fucheng680519@163.com>
2025-12-11 17:26:36 +0800

    debug

--- pod-scheduler-service/xpu-scheduler-plugin/build/build.sh
+++ pod-scheduler-service/xpu-scheduler-plugin/build/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 BASE_VERSION='1.9.0'
-GOPATH=$(GOPATH:-$(go env GOPATH))
+GOPATH=${GOPATH:-$(go env GOPATH)}
 TOP_DIR=${GOPATH}/src/volcano.sh/volcano
 BASE_PATH=${GOPATH}/src/volcano.sh/volcano/pkg/scheduler/plugins/xpu-scheduler-plugin/
 CMD_PATH=${GOPATH}/src/volcano.sh/volcano/cmd
 PKG_PATH=volcano.sh/volcano/pkg
-DATE=$(date +%Y-%m-%d %H:%M:%S)
 GCC_PATH=/usr/bin/gcc
 
 PLUGIN_NAME=huawei-xpu
@@ -30,21 +29,21 @@ function build() {
 
   cd "${BASE_PATH}"/output
 
-  for name in controller-manager scheduler webhookmanager; do
+  for name in controller-manager scheduler webhook-manager; do
     CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" \
-    CGO_CPPLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" \
+       CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" \
     CC=${GCC_PATH} CGO_ENABLED=1 \
-    go build -mod=mod -buildmode=pie -ldflags="-s -linkmode=external -extldflags=-Wl,-z,relro.-z,now
-    -X '${PKG_PATH}/version.Built=${DATE}' -X '${PKG_PATH}/version.Version=${BASE_VERSION}'" \
+    go build -mod=mod -buildmode=pie -ldflags="-s -linkmode=external -extldflags=-Wl,-z,relro,-z,now
+    -X '${PKG_PATH}/version.Version=${BASE_VERSION}'" \
     -o vc-${name} ${CMD_PATH}/$name
   done
 
   CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" \
   CGO_CPPLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv" \
   CC=${GCC_PATH} CGO_ENABLED=1 \
-  go build -mod=mod -buildmode=plugin -ldflags="-s -linkmode=external -extldflags=-Wl,-z,relro.-z,now
-  -X volcano.sh/volcano/pkg/plugins/xpu-scheduler-plugin.PluginName=${PLUGIN_NAME}" \
-  -o "${PLUGIN_NAME}".so "${GOPATH}"/src/volcano.sh/volcano/pkg/plugins/xpu-scheduler-plugin/
+  go build -mod=mod -buildmode=plugin -ldflags="-s -linkmode=external -extldflags=-Wl,-z,relro,-z,now
+  -X volcano.sh/volcano/pkg/scheduler/plugins/xpu-scheduler-plugin.PluginName=${PLUGIN_NAME}" \
+  -o "${PLUGIN_NAME}".so "${GOPATH}"/src/volcano.sh/volcano/pkg/scheduler/plugins/xpu-scheduler-plugin/
 
   if [ ! -f "${BASE_PATH}/output/${PLUGIN_NAME}.so" ]
   then
