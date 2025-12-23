b27cd86f40b2e9894a786cc6dcf9870d8f9770bd
Yifu_Liu <787737563@qq.com>
2025-12-23 18:42:01 +0800

    1

--- xpu-pool-service/GPU-device-plugin/pkg/api/runtime/service/service_impl.go
+++ xpu-pool-service/GPU-device-plugin/pkg/api/runtime/service/service_impl.go
@@ -24,6 +24,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 
+	"huawei.com/xpu-device-plugin/pkg/log"
 	"huawei.com/xpu-device-plugin/pkg/plugin/config"
 	"huawei.com/xpu-device-plugin/pkg/plugin/types"
 	"huawei.com/xpu-device-plugin/pkg/plugin/util"
