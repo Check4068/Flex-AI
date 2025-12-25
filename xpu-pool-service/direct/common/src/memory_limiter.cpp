5778b960a23296fac04e04a75d87c3f96780c569
Mori-yuwen <fucheng680519@163.com>
2025-12-25 16:24:04 +0800

    Update memory_limiter.cpp

--- xpu-pool-service/direct/common/src/memory_limiter.cpp
+++ xpu-pool-service/direct/common/src/memory_limiter.cpp
@@ -14,13 +14,13 @@ bool MemoryLimiter::MemoryCheck(size_t requested)
     size_t used;
     int ret = xpu_.MemoryUsed(used);
     if (ret) {
-        log_err("get used memory failed, ret is {%d}", ret);
+        log_err("get used memory failed, ret is {}", ret);
         return false;
     }
 
     size_t quota = config_.MemoryQuota();
     if (requested + used > quota) {
-        log_err("out of memory, request {%lld}, used {%lld}, quota {%lld}",
+        log_err("out of memory, request {} B, used {} B, quota {} B",
             requested, used, quota);
         return false;
     }
@@ -37,10 +37,10 @@ int MemoryLimiter::CreateFileLockBaseDir()
 {
     int ret = mkdir(FILELOCK_BASE_DIR.c_str(), S_IRWXU | S_IRGRP | S_IXGRP);
     if (ret < 0 && errno != EEXIST) {
-        log_err("mkdir {%s} failed, err is {%d}", FILELOCK_BASE_DIR, strerror(errno));
+        log_err("mkdir {} failed, err is {}", FILELOCK_BASE_DIR, strerror(errno));
         return RET_FAIL;
     }
-    log_info("mkdir {%s} succ", FILELOCK_BASE_DIR);
+    log_info("mkdir {} succ", FILELOCK_BASE_DIR);
     return RET_SUCC;
 }
 
